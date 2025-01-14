aff2d26e22c5faf9de5acf26b008fc1b5d9a8d2c
YuzhaninDaniil <159440936+YuzhaninDaniil@users.noreply.github.com>
2025-01-15 01:38:36 +0700

    Update Main.h

--- src/Laboratory4/Laboratory4/Main.h
+++ src/Laboratory4/Laboratory4/Main.h
@@ -3,27 +3,25 @@
 #include "Dictionary.h"
 
 /// <summary>
-/// ������� ��� �������� ����� ������ ����� �������������.
-/// ������� ����������� � ������������ ���� ������ ����� � ���������� ������ �� ��� ���,
-/// ���� �� ����� ������� ���������� ��������.
+/// Функция для проверки ввода целого числа пользователем.
 /// </summary>
-/// <returns>��������� ������������� ����� �����.</returns>
+/// <returns>Введенное пользователем целое число.</returns>
 int CheckData();
 
 /// <summary>
-/// ����� ���-�������.
+/// Вывод хэш-таблицы.
 /// </summary>
-/// <param name="dict">�������.</param>
+/// <param name="dict">Словарь.</param>
 void PrintTable(const Dictionary& dict);
 
 /// <summary>
-/// ����� �������.
+/// Вывод словаря.
 /// </summary>
-/// <param name="dict">�������.</param>
+/// <param name="dict">Словарь.</param>
 void PrintDictionary(const Dictionary& dict);
 
 /// <summary>
-/// ����� ����� �  ���������.
+/// Точка входа в программу.
 /// </summary>
-/// <returns>�����.</returns>
+/// <returns>Число.</returns>
 int main();
