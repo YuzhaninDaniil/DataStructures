052a3b6f046875146dd57f70196f4385006420d8
YuzhaninDaniil <159440936+YuzhaninDaniil@users.noreply.github.com>
2025-01-15 02:08:54 +0700

    Update Treap.h

--- src/Laboratory5/Laboratory5/Treap.h
+++ src/Laboratory5/Laboratory5/Treap.h
@@ -3,12 +3,12 @@
 #include "TreapNode.h"
 
 /// <summary>
-/// ��������� ������.
+/// Декартово дерево.
 /// </summary>
 struct Treap
 {
 	/// <summary>
-	/// ��������� �� �������� ���� ������.
+	/// Указатель на корневой узел дерева.
 	/// </summary>
 	TreapNode* Root;
 
@@ -16,63 +16,63 @@ struct Treap
 };
 
 /// <summary>
-/// ����� �������� � ���������� ������.
+/// Поиск элемента в декартовом дереве.
 /// </summary>
-/// <param name="node">���� ������</param>
-/// <param name="key">������� ��������.</param>
+/// <param name="node">Узел дерева</param>
+/// <param name="key">Искомое значение.</param>
 /// <returns></returns>
 TreapNode* FindElement(TreapNode* node, int key);
 
 /// <summary>
-/// ���������� ������ �� ��� ������ �� ��������.
+/// Разделение дерева на два дерева по значению
 /// </summary>
-/// <param name="node">���� ������.</param>
-/// <param name="key">�������� ��� ����������.</param>
-/// <param name="left">����� ������.</param>
-/// <param name="right">������ ������.</param>
+/// <param name="node">Узел дерева.</param>
+/// <param name="key">Критерий для разделения.</param>
+/// <param name="left">Левое дерево</param>
+/// <param name="right">Правое дерево.</param>
 void Split(TreapNode* node, int key, TreapNode*& left, TreapNode*& right);
 
 /// <summary>
-/// ������� ���� �������� � ����.
+/// Слияние двух деревьев в одно
 /// </summary>
-/// <param name="left">����� ������.</param>
-/// <param name="right">������ ������.</param>
+/// <param name="left">Левое дерево</param>
+/// <param name="right">Правое дерево</param>
 /// <returns></returns>
 TreapNode* Merge(TreapNode* left, TreapNode* right);
 
 /// <summary>
-/// ������� ���������� ���� � ������ ������������ ������������������ ��������.
+/// Функция добавление узла в дерево использующая неоптимизированный алгоритм
 /// </summary>
-/// <param name="node">���� ������.</param>
-/// <param name="key">��������.</param>
+/// <param name="node">Узел дерева</param>
+/// <param name="key">Значение</param>
 void AddUnoptimized(TreapNode*& node, int key);
 
 /// <summary>
-///  ������� ���������� ���� � ������ ������������ ���������������� ��������.
+///  Функция добавления узла в дерево использующая оптимизированный алгоритм
 /// </summary>
-/// <param name="node">���� ������.</param>
-/// <param name="key">��������.</param>
+/// <param name="node">Узел дерева</param>
+/// <param name="key">Значение</param>
 void AddOptimized(TreapNode*& node, int key);
 
 /// <summary>
-/// ������� �������� ����� �� ������ c ������ ������, ���������� ������������������ ��������.
+/// Функция удаления узлов из дерева c равным ключем, использует неоптимизированный алгоритм
 /// </summary>
-/// <param name="node">���� ������.</param>
-/// <param name="key">��������.</param>
-/// <param name="success">������ ��������.</param>
+/// <param name="node">Узел дерева</param>
+/// <param name="key">Значение</param>
+/// <param name="success">Статус удаления</param>
 void RemoveUnoptimized(TreapNode*& node, int key, bool& success);
 
 /// <summary>
-/// ������� �������� ����� �� ������ c ������ ������, ���������� ��������������� ��������.
+/// Функция удаления узлов из дерева c равным ключем, использует птимизированный алгоритм
 /// </summary>
-/// <param name="node">���� ������.</param>
-/// <param name="key">��������.</param>
-/// <param name="success">������ ��������.</param>
+/// <param name="node">Узел дерева</param>
+/// <param name="key">Значение</param>
+/// <param name="success">Статус удаления</param>
 void RemoveOptimized(TreapNode*& node, int key, bool& success);
 
 /// <summary>
-/// ���������� Key (�������� ����).
+/// Получить значение.
 /// </summary>
-/// <param name="key">��������.</param>
-/// <returns>���������� ��������.</returns>
+/// <param name="key"></param>
+/// <returns></returns>
 int GetValue(TreapNode* node, int key);
