024b60a907d7d93671908933ba20e00825e62def
YuzhaninDaniil <159440936+YuzhaninDaniil@users.noreply.github.com>
2025-01-15 02:13:51 +0700

    Update BinaryTreeNode.h

--- src/Laboratory5/Laboratory5/BinaryTreeNode.h
+++ src/Laboratory5/Laboratory5/BinaryTreeNode.h
@@ -1,27 +1,27 @@
 #pragma once
 /// <summary>
-/// ���� ��������� ������.
+/// Узел бинарного дерева.
 /// </summary>
 struct BinaryTreeNode
 {
 	/// <summary>
-	/// ��������� �� ����� ����.
+	/// Указатель на левый  дочерний узел.
 	/// </summary>
 	BinaryTreeNode* Left = nullptr;
 
 	/// <summary>
-	/// ��������� �� ������ ����.
+	/// Указатель на правый  дочерний узел.
 	/// </summary>
 	BinaryTreeNode* Right = nullptr;
 
 	/// <summary>
-	/// ������, ������� �������� ����.
+	/// Данные, которые содержит узел.
 	/// </summary>
 	int Data;
 
 	/// <summary>
-	/// �����������.
+	/// Конструктор.
 	/// </summary>
-	/// <param name="value">������� ����.</param>
+	/// <param name="value">Знаачение элемента.</param>
 	BinaryTreeNode(int value) : Left(nullptr), Right(nullptr), Data(value) {}
 };
