d675f5a7ddf8c8525c3fde314b80e21d1c4d4e07
YuzhaninDaniil <159440936+YuzhaninDaniil@users.noreply.github.com>
2025-01-15 02:06:45 +0700

    Update TreapNode.h

--- src/Laboratory5/Laboratory5/TreapNode.h
+++ src/Laboratory5/Laboratory5/TreapNode.h
@@ -2,38 +2,38 @@
 #include <random>
 
 /// <summary>
-/// ���� ����������� ������.
+/// Узел декартового дерева.
 /// </summary>
 struct TreapNode
 {
 	/// <summary>
-	/// �������� ����.
+	/// Значение узла.
 	/// </summary>
 	int Key;
 
 	/// <summary>
-	/// ���������  ����.
+	/// Приоритет узла.
 	/// </summary>
 	int Priority;
 
 	/// <summary>
-	/// ��������� �� ����� ����.
+	/// Указатель на левый узел.
 	/// </summary>
 	TreapNode* LeftNode = nullptr;
 
 	/// <summary>
-	/// ��������� �� ������ ����.
+	/// Указатель на правый узел.
 	/// </summary>
 	TreapNode* RightNode = nullptr;
 
 	/// <summary>
-	/// ����������� ���� � �������������� Key � ��������� �����������.
+	/// Конструктор узла с инициализацией Key и случайным приоритетом.
 	/// </summary>
-	/// <param name="key">���� ����</param>
+	/// <param name="key">Ключ узла.</param>
 	TreapNode(int key)
 	{
 		Key = key;
-		//������������ ��������� � ������� �������.
+		//Используем рандом для приоритета.
 		static std::mt19937 random(std::random_device{}());
 		Priority = std::uniform_int_distribution<int>(1, 1000)(random);
 		LeftNode = nullptr;
