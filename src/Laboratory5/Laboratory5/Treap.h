e70612972d863094ab690e3c3fd33b9e08e7b49b
YuzhaninDaniil <159440936+YuzhaninDaniil@users.noreply.github.com>
2025-01-15 02:10:43 +0700

    Update Treap.h

--- src/Laboratory5/Laboratory5/Treap.h
+++ src/Laboratory5/Laboratory5/Treap.h
@@ -18,61 +18,61 @@ struct Treap
 /// <summary>
 /// Поиск элемента в декартовом дереве.
 /// </summary>
-/// <param name="node">Узел дерева</param>
+/// <param name="node">Узел дерева.</param>
 /// <param name="key">Искомое значение.</param>
 /// <returns></returns>
 TreapNode* FindElement(TreapNode* node, int key);
 
 /// <summary>
-/// Разделение дерева на два дерева по значению
+/// Разделение дерева на два дерева по значению.
 /// </summary>
 /// <param name="node">Узел дерева.</param>
 /// <param name="key">Критерий для разделения.</param>
-/// <param name="left">Левое дерево</param>
+/// <param name="left">Левое дерево.</param>
 /// <param name="right">Правое дерево.</param>
 void Split(TreapNode* node, int key, TreapNode*& left, TreapNode*& right);
 
 /// <summary>
-/// Слияние двух деревьев в одно
+/// Слияние двух деревьев в одно.
 /// </summary>
-/// <param name="left">Левое дерево</param>
-/// <param name="right">Правое дерево</param>
+/// <param name="left">Левое дерево.</param>
+/// <param name="right">Правое дерево<./param>
 /// <returns></returns>
 TreapNode* Merge(TreapNode* left, TreapNode* right);
 
 /// <summary>
-/// Функция добавление узла в дерево использующая неоптимизированный алгоритм
+/// Функция добавление узла в дерево использующая неоптимизированный алгоритм.
 /// </summary>
-/// <param name="node">Узел дерева</param>
-/// <param name="key">Значение</param>
+/// <param name="node">Узел дерева.</param>
+/// <param name="key">Значение.</param>
 void AddUnoptimized(TreapNode*& node, int key);
 
 /// <summary>
-///  Функция добавления узла в дерево использующая оптимизированный алгоритм
+///  Функция добавления узла в дерево использующая оптимизированный алгоритм.
 /// </summary>
-/// <param name="node">Узел дерева</param>
-/// <param name="key">Значение</param>
+/// <param name="node">Узел дерева.</param>
+/// <param name="key">Значение.</param>
 void AddOptimized(TreapNode*& node, int key);
 
 /// <summary>
-/// Функция удаления узлов из дерева c равным ключем, использует неоптимизированный алгоритм
+/// Функция удаления узлов из дерева c равным ключем, использует неоптимизированный алгоритм.
 /// </summary>
-/// <param name="node">Узел дерева</param>
-/// <param name="key">Значение</param>
-/// <param name="success">Статус удаления</param>
+/// <param name="node">Узел дерева.</param>
+/// <param name="key">Значение.</param>
+/// <param name="success">Статус удаления.</param>
 void RemoveUnoptimized(TreapNode*& node, int key, bool& success);
 
 /// <summary>
-/// Функция удаления узлов из дерева c равным ключем, использует птимизированный алгоритм
+/// Функция удаления узлов из дерева c равным ключем, использует птимизированный алгоритм.
 /// </summary>
-/// <param name="node">Узел дерева</param>
-/// <param name="key">Значение</param>
-/// <param name="success">Статус удаления</param>
+/// <param name="node">Узел дерева.</param>
+/// <param name="key">Значение.</param>
+/// <param name="success">Статус удаления.</param>
 void RemoveOptimized(TreapNode*& node, int key, bool& success);
 
 /// <summary>
 /// Получить значение.
 /// </summary>
-/// <param name="key"></param>
-/// <returns></returns>
+/// <param name="key">Ключ</param>
+/// <returns>Значение</returns>
 int GetValue(TreapNode* node, int key);
