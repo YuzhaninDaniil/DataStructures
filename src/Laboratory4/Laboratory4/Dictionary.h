56c2a164b73bd16f0dd61519f9e0d57abba35aa4
YuzhaninDaniil <159440936+YuzhaninDaniil@users.noreply.github.com>
2025-01-15 01:48:01 +0700

    Update Dictionary.h

--- src/Laboratory4/Laboratory4/Dictionary.h
+++ src/Laboratory4/Laboratory4/Dictionary.h
@@ -3,62 +3,61 @@
 #include "HashTable.h"
 #include <iostream>
 
-// TODO: Полетела кодировка. Посмотреть везде
 /// <summary>
-/// ����� ��� ������ �� �������.
+/// Класс для работы со словарём.
 /// </summary>
 class Dictionary
 {
 private:
 	/// <summary>
-	/// ���-������� ��� �������� ������.
+	/// Хэш-таблица для хранения данных.
 	/// </summary>
 	HashTable _dictionary;
 
 public:
 	/// <summary>
-	/// ����������� �������.
+	/// Конструктор словаря.
 	/// </summary>
-	/// <param name="initialSize">������ �������.</param>
-	/// <param name="loadFactor">������������ ���������� ����������.</param>
+	/// <param name="initialSize">Размер словаря.</param>
+	/// <param name="loadFactor">Максимальный коэфициент заполнения.</param>
 	Dictionary(size_t initialSize = InitialTableSize, double loadFactor = MaxLoadFactor);
 
 	/// <summary>
-	/// ��������� ������� � �������.
+	/// Добавляет элемент в словарь.
 	/// </summary>
-	/// <param name="key">����.</param>
-	/// <param name="value">�������� ��������.</param>
+	/// <param name="key">Ключ.</param>
+	/// <param name="value">Значение элемента.</param>
 	void Insert(const std::string& key, const std::string& value);
 
 	/// <summary>
-	/// �������� �������� �� �����.
+	/// Удаление элементе по ключу.
 	/// </summary>
-	/// <param name="key">����.</param>
+	/// <param name="key">Ключ.</param>
 	void Remove(const std::string& key);
 
 	/// <summary>
-	/// ����� �������� �� �����.
+	/// Поиск значения по ключу.
 	/// </summary>
-	/// <param name="key">����.</param>
-	/// <returns>������� ��������.</returns>
+	/// <param name="key">Ключ.</param>
+	/// <returns>Искомое значение.</returns>
 	string Find(const std::string& key);
 
 	/// <summary>
-	/// ���������� ������ �������.
+	/// Возвращает размер словаря.
 	/// </summary>
-	/// <returns>������ �������.</returns>
+	/// <returns>Размер таблицы.</returns>
 	size_t GetSize() const;
 
 	/// <summary>
-	/// ���������� ���������� ���������.
+	/// Возвращает количество элементов.
 	/// </summary>
-	/// <returns>���������� ���������.</returns>
+	/// <returns>Количество элементов.</returns>
 	size_t GetCount() const;
 
 	/// <summary>
-	/// ���������� ����� �� ������� � �������.
+	/// Возвращает бакет по индексу в словаре.
 	/// </summary>
-	/// <param name="index">���������� ������.</param>
-	/// <returns>������� �����.</returns>
+	/// <param name="index">Получаемый индекс.</param>
+	/// <returns>Искомый бакет.</returns>
 	Node* GetBucket(size_t index) const;
 };
