be56cf099b63492d3440f9c23b3336c1e047fbf5
YuzhaninDaniil <159440936+YuzhaninDaniil@users.noreply.github.com>
2025-01-15 01:35:14 +0700

    Update Node.h

--- src/Laboratory4/Laboratory4/Node.h
+++ src/Laboratory4/Laboratory4/Node.h
@@ -5,29 +5,29 @@
 using namespace std;
 
 /// <summary>
-/// ��������� ��� �������� ���� ����-��������.
+/// Структура для хранения пары ключ-значение.
 /// </summary>
 struct Node
 {
 	/// <summary>
-	/// ����.
+	/// Ключ.
 	/// </summary>
 	string key;
 
 	/// <summary>
-	/// ��������.
+	/// Значение.
 	/// </summary>
 	string value;
 
 	/// <summary>
-	/// ��������� �� ��������� ����.
+	/// Указатель на следующую пару.
 	/// </summary>
 	Node* next;
 
 	/// <summary>
-	/// ����������� �������� ������� (����-��������).
+	/// Конструктор элемента словаря (ключ-значение).
 	/// </summary>
-	/// <param name="k">����.</param>
-	/// <param name="v">��������.</param>
+	/// <param name="k">Ключ.</param>
+	/// <param name="v">Значение.</param>
 	Node(const string& k, const string v) : key(k), value(v), next(nullptr) {}
 };
